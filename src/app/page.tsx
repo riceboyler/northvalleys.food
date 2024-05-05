0550f3b8e473cbe4fd2d377f7aebb71b3ebb885e
Jason Clark <riceboyler@gmail.com>
2024-05-05 11:05:42 -0700

    Initial View (ugly as it is) built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,83 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+import { QueryResult, QueryData } from '@supabase/supabase-js';
+import { createClient } from '~/utils/supabase/server';
+import { cookies } from 'next/headers';
+import dayjs from 'dayjs';
+import { Card, Table } from '../components/ParkUI';
+import { Flex, Grid } from '../../styled-system/jsx';
+
+type ScheduleRow = {
+  date: Date;
+  start_time: string;
+  end_time: string;
+  location: {
+    name: string;
+    address: string;
+  };
+  truck: {
+    name: string;
+  };
+};
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const scheduleWithLocationTruck = await supabase.from('schedule').select(`
+    date,
+    start_time,
+    end_time,
+    location (
+      name,
+      address
+    ),
+    truck (
+      name
+    )
+    `).returns<ScheduleRow[]>();
+
+  type ScheduleWithLocationTruck = QueryData<typeof scheduleWithLocationTruck>;
+  const schedule = scheduleWithLocationTruck.data;
+
+  const todaySchedule = schedule?.filter((row) => dayjs(row.date).isSame(dayjs(), 'day'));
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <h1>Today's Trucks</h1>
+      <Flex direction={{ base: 'column', md: 'row' }} gap="16px" justifyContent="center" alignItems="center">
+        {todaySchedule?.map((row) => (
+          <Card.Root width="sm" key={row.truck.name}>
+            <Card.Header>
+              <Card.Title>{row.truck.name}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <h6>{row.start_time} til {row.end_time}</h6>
+              {row.location.name} - {row.location.address}
+            </Card.Body>
+          </Card.Root>
+        ))}
+      </Flex>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <h1>Full Schedule</h1>
+      <Table.Root colorPalette="accent">
+        <Table.Caption>North Valleys Food Truck Schedule</Table.Caption>
+        <Table.Head>
+          <Table.Row fontWeight="bold">
+            <Table.Header>Location</Table.Header>
+            <Table.Header>Date</Table.Header>
+            <Table.Header>Truck</Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {schedule?.map((row) => (
+            <Table.Row fontWeight={dayjs(row.date).isSame(dayjs(), 'day') ? 'bold' : 'initial'} key={`${dayjs(row.date).format('MM/DD')} - ${row.truck.name}`}>
+              <Table.Cell>{row.location?.name}</Table.Cell>
+              <Table.Cell>{dayjs(row.date).format('ddd, MMM D')}<br />{row.start_time} - {row.end_time}</Table.Cell>
+              <Table.Cell>{row.truck?.name}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 }
